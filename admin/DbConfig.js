8ade9bb05d50bb5a3a01a1ef30cfc20ffcf967d1
Chris Murphy <cdmurphy@seas.upenn.edu>
2023-05-24 22:19:54 -0400

    Update DbConfig.js
    Changed localhost to 127.0.0.1

--- admin/DbConfig.js
+++ admin/DbConfig.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 // the host:port must match the location where you are running MongoDB
 // the "myDatabase" part can be anything you like
-mongoose.connect('mongodb://localhost:27017/myDatabase');
+mongoose.connect('mongodb://127.0.0.1:27017/myDatabase');
 
 var Schema = mongoose.Schema;
 
